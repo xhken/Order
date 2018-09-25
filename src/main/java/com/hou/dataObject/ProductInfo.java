d3b3163b4b9c9e8ecca8294e12419ad1b87ee843
Ken <sxhwork@126.com>
2018-09-25 17:25:15 +0800

    Update

--- src/main/java/com/hou/dataObject/ProductInfo.java
+++ src/main/java/com/hou/dataObject/ProductInfo.java
@@ -1,11 +1,12 @@
 package com.hou.dataObject;
 
 import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import java.math.BigDecimal;
-import java.util.Date;
 
 /**
  * Created by Ken on 2018/9/19.
@@ -13,13 +14,15 @@ import java.util.Date;
 @Table(name = "product_info")
 @Entity
 @Data
+@DynamicUpdate
 public class ProductInfo {
+    @Id
     private String productId;
     private String productName;
     private BigDecimal productPrice;
     private int productStock;
     private String productIcon;
-    private String productStatus;
+    private int productStatus;
     private int categoryType;
 //    private Date createTime;
 //    private Date updateTime;
--- src/test/java/com/hou/dao/ProductInfoRepositoryTest.java
+++ src/test/java/com/hou/dao/ProductInfoRepositoryTest.java
@@ -1,14 +1,23 @@
 package com.hou.dao;
 
+import com.hou.dataObject.ProductCategory;
 import com.hou.dataObject.ProductInfo;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by Ken on 2018/9/21.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ProductInfoRepositoryTest {
     @Autowired
     private ProductInfoRepository repository;
@@ -17,6 +26,14 @@ public class ProductInfoRepositoryTest {
     public void saveTest()
     {
         ProductInfo productInfo = new ProductInfo();
-        productInfo.setProductInfo("12345");
+        productInfo.setProductId("123");
+        productInfo.setProductName("豆沙包");
+        productInfo.setProductPrice(BigDecimal.valueOf(9.00));
+        productInfo.setProductStock(100);
+        productInfo.setProductIcon("http://www.baidu.com");
+        productInfo.setProductStatus(1);
+        productInfo.setCategoryType(101);
+        ProductInfo result = repository.save(productInfo);
+        Assert.assertNotNull(result);
     }
 }
